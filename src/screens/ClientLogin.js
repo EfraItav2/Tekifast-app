05d123e28a228fd78fe2d9957a6dbdfe68dd38af
Jorge David Vilchis Manzano <79020533+DavidVilchis@users.noreply.github.com>
2022-04-08 07:22:05 -0500

    Pantalla de Selección y Login de Cliente
    Creación de la pantalla de selección de usuario y de cliente

---
+++ src/screens/ClientLogin.js
@@ -0,0 +1,91 @@
+import { Icon, Text, NativeBaseProvider, Box, Heading, HStack, VStack, FormControl, Input, Button, Center, Link, extendTheme, Fab } from 'native-base';
+import React from 'react';
+import { Fontisto, Ionicons, AntDesign } from "@expo/vector-icons";
+
+const Form = () => {
+    return <Center w="100%" h="100%" bg="primary.50">
+        <Box safeArea p="2" py="8" w="100%" bg="primary.50">
+            <Heading textAlign="center" size="lg" fontWeight="600" color="coolGray.800" _dark={{
+                color: "warmGray.50"
+            }}>
+                I'm Client
+            </Heading>
+            <Heading textAlign="center" mt="2" _dark={{
+                color: "warmGray.200"
+            }} color="coolGray.600" fontWeight="medium" size="xs">
+                Sign in to continue!
+            </Heading>
+
+            <VStack space={3} mt="5" bg="primary.50">
+                <FormControl>
+                    <FormControl.Label>Email</FormControl.Label>
+                    <Input placeholder="example@email.com" borderColor={"black"}/>
+                </FormControl>
+                <FormControl>
+                    <FormControl.Label>Password</FormControl.Label>
+                    <Input type="password" placeholder="Enter your password" borderColor={"black"} />
+                    <Link _text={{
+                        fontSize: "xs",
+                        fontWeight: "500",
+                        color: "indigo.500"
+                    }} alignSelf="flex-end" mt="1">
+                        Forget Password?
+                    </Link>
+                </FormControl>
+                <Button mt="2" bg="primary.400">
+                    Sign in
+                </Button>
+                <Button mt="1" bg="primary.800" leftIcon={<Icon as={AntDesign} name="google" size="sm" />}>
+                    Sign in with Google
+                </Button>
+                <HStack mt="6" justifyContent="center">
+                    <Text fontSize="sm" color="coolGray.600" _dark={{
+                        color: "warmGray.200"
+                    }}>
+                        I'm a new user.{" "}
+                    </Text>
+                    <Link _text={{
+                        color: "indigo.500",
+                        fontWeight: "medium",
+                        fontSize: "sm"
+                    }} href="#">
+                        Sign Up
+                    </Link>
+                </HStack>
+            </VStack>
+        </Box>
+    </Center>;
+};
+const ClientLogin = ({navigation}) => {
+    const theme = extendTheme({
+        colors: {
+            primary: {
+                50: '#e0fded',
+                100: '#bbf2d3',
+                200: '#93e9b7',
+                300: '#6adf9b',
+                400: '#42d680',
+                500: '#29bd66',
+                600: '#1d934f',
+                700: '#116938',
+                800: '#054020',
+                900: '#001705'
+            }
+        }
+    })
+    return (
+        <NativeBaseProvider theme={theme}>
+            <Box position="relative" justifyContent={"right"} bg="primary.50">
+                <Box position="relative" >
+                    <Button mt={"3"} mr={"3"} alignSelf="flex-end" variant="ghost" leftIcon={<Icon as={Fontisto} name="world-o" size="sm" />}>
+                        ENG
+                    </Button>
+                </Box>
+            </Box>
+            <Form />
+            <Fab onPress={()=>navigation.goBack()} renderInPortal={false} shadow={2} size="4" placement="top-left" icon={<Icon color="white" as={Ionicons} name="chevron-back" size="4" />} />
+        </NativeBaseProvider>
+    )
+}
+
+export default ClientLogin;
---
+++ src/screens/UserSelectScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { NativeBaseProvider, View, Center, Heading, Button, HStack, VStack, extendTheme, Stagger, useDisclose, IconButton, Icon, Box, Menu } from 'native-base';
+import { Fontisto } from "@expo/vector-icons";
+
+const UserSelectScreen = ({ navigation }) => {
+    const theme = extendTheme({
+        colors: {
+            primary: {
+                50: '#e0fded',
+                100: '#bbf2d3',
+                200: '#93e9b7',
+                300: '#6adf9b',
+                400: '#42d680',
+                500: '#29bd66',
+                600: '#1d934f',
+                700: '#116938',
+                800: '#054020',
+                900: '#001705'
+            }
+        }
+    })
+    return (
+        <NativeBaseProvider theme={theme}>
+            <Box bg="primary.50">
+                <Button mt={"2"} ml={"2"} width={"25%"} variant="ghost" leftIcon={<Icon as={Fontisto} name="world-o" size="sm" />}>
+                    ENG
+                </Button>
+            </Box>
+            <Center bg="primary.50" flex={1} px="3">
+                <View>
+                    <Heading color={"primary.700"} size="2xl" >Welcome<Heading color={"primary.600"} size="2xl">!</Heading></Heading>
+                    <Heading color={"primary.800"} size="md" textAlign="center">How do you want to log in?</Heading>
+                    <VStack space={2} pt={5} alignItems="center">
+                        <Button bg={"primary.600"} width="100%" onPress={() => navigation.navigate('ClientLogin')}>CLIENT</Button>
+                        <Button bg={"primary.400"} width="100%">PROFESSIONAL</Button>
+                    </VStack>
+                </View>
+            </Center>
+        </NativeBaseProvider>
+    )
+}
+
+export default UserSelectScreen;
